f1ce94211eca09790b09fc9d4013d03b90b460cb
Documenter.jl <documenter@juliadocs.github.io>
2025-02-20 17:57:28 +0000

    build based on fbc8bd9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UnivariateApprox","category":"page"},{"location":"#UnivariateApprox","page":"Home","title":"UnivariateApprox","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UnivariateApprox.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UnivariateApprox]","category":"page"}]
+}
