a686b8162005dbb8354576f20f8180e91073ad46
Daniel Sharp <dannys4@mit.edu>
2025-02-20 14:58:07 -0500

    Bump julia compat, add testset

--- test/gausslaguerre_lobatto.jl
+++ test/gausslaguerre_lobatto.jl
@@ -1,12 +1,14 @@
-for n in 1:18
-    pts, wts = gausslaguerre_lobatto(n)
-    evals = ones(n)
-    for degree in 0:2n-2
-        monomial_int = evals'wts
-        exact_int = factorial(big(degree))
-        # Manually set relative tolerance as larger for high-degree polynomials due to numerical issues
-        rtol = degree < 13 && n < 13 ? 5e-12 : 1e-8
-        @test monomial_int ≈ exact_int rtol=rtol
-        evals .*= pts
+@testset "Gauss-Laguerre-Lobatto quadrature" begin
+    for n in 1:18
+        pts, wts = gausslaguerre_lobatto(n)
+        evals = ones(n)
+        for degree in 0:2n-2
+            monomial_int = evals'wts
+            exact_int = factorial(big(degree))
+            # Manually set relative tolerance as larger for high-degree polynomials due to numerical issues
+            rtol = degree < 13 && n < 13 ? 5e-12 : 1e-8
+            @test monomial_int ≈ exact_int rtol=rtol
+            evals .*= pts
+        end
     end
 end
